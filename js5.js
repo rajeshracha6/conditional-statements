ac888d0c9cd4198dd23dfb4275342caa70b6ee55
rajeshracha6 <rajeshracha752@gmail.com>
2024-08-10 23:03:57 +0530

    files

---
+++ js5.js
@@ -0,0 +1,224 @@
+
+// wraite a  pogram to print the square of each number from 1 to 10
+
+    // print the squares from 1 t0 10
+
+    
+    // for (let i = 1; i <= 10; i++) {
+    //     let square = i * i;
+    //     console.log("Square of " + i + " is: " + square);
+    // }
+
+    // let sum = 0; //dout
+
+    // for (let i = 1; i <=20; i++) {
+
+       
+    //     if( i%3 ===0){
+    //         sum=sum+i;
+    //     }
+    // }
+    // console.log(sum);
+
+    //     console.log("The sum of numbers from 1 to 10 that are divisible by 3 is: " + sum) }
+        
+    // let cube =0;
+    // for (let i = 1; i <= 15; i++) {
+    //     let cube = i * i * i;
+    //     console.log("Cube of " + i + " is: " + cube);}
+
+
+    // for(i=20;i>0;i--){
+    // console.log(i)}
+
+
+    // for(i=1;i<12;i++){
+    //     let multification = 1*2*3*4*5*6*7*8*9*10*11*12;
+    //     console.log(multification);
+
+
+    // for (i=20;i>0;i-=5)
+    //     {     
+    //     console.log(i);
+    // }
+
+// let i=1
+//     for(i=1;i<=20;i++)
+//     {
+//         if(i%5===0)
+//         {
+            
+//             continue;
+            
+//         }
+//         else{
+//             console.log(i);
+//         }
+        
+//     }
+    
+    
+//     let sum=0; *(dout)
+//     for(let i=1;i<=30;i++){
+//         if(i%4===0){
+//          sum+=i;
+//     }
+// }
+// console.log("The sum of numbers divisible by 4 from 1 to 30 is: " + sum);
+
+    // for(i=50; i>=1;i--)
+
+    //     if
+    //         (i%3===0){
+    //             console.log(i);
+    //         }
+
+    // simple clculater
+    
+    // let =prompt ("10");
+    // let =prompt("20");
+    // let operator = '+';
+    // let result = 0;
+    // if(operator === '+');
+    // result = prompt + prompt;
+    // console.log(result);
+
+
+    // x=prompt(10);
+    // y=prompt(20);
+    // operator=prompt("+");
+    // if(operator==="+");
+    // result=x+y;
+    // console.log(result);
+
+
+    // let x=prompt(10);
+    // let y=prompt(20);
+    // let operator=prompt("+");
+    // let result=0;
+    // if(operator==="+");
+    // let=x+y;
+    // alert(result);
+
+
+//     let x=+prompt("enter 1st operand");
+//     let y=+prompt("enter 2nd operand");
+//     let operator=prompt("enter operator:");
+//     let result;
+//     if(operator==="+"){
+//     result=x+y;
+//     alert(result);}
+
+//     else {
+//         if(operator==="-"){
+//             result=x-y;
+//             alert(result);
+//     }
+// }
+
+// function name(x,y,operator) {
+//     if(operator=="+"){
+//         console.log(x+y)
+// }  
+// if(operator=="*"){
+//     console.log(x*y)
+// }
+// if(operator=="/"){
+//     console.log(x/y)
+// }
+// if(operator=="%"){
+//     console.log(x%y)
+// }
+
+// else{
+//     console.log("invalid operator")
+// }
+    
+// }
+// name(7,3,'/')
+
+
+// let x=+prompt("enter first operand");
+// let y=+prompt("enter second operand");
+// let operator=prompt("enter operator");
+// result;
+
+//   if(operator==="+"){
+//     result=x+y;
+//     alert(result);
+
+//   }
+
+
+
+    
+
+
+    // let num1 = 10;
+    // let num2 = 20;
+    // let operator = '-';
+    // let result = 0;
+    // if(operator === '-');
+    // result = num1 - num2;
+    // console.log(result);
+
+    // let num1 = 10;
+    // let num2 = 20;
+    // let operator = '*';
+    // let result = 0;
+    // if(operator === '*');
+    // result = num1 * num2;
+    // console.log(result);
+
+    // let num1 = 10;
+    // let num2 = 20;
+    // let operator = '%';
+    // let result = 0;
+    // if(operator === '%');
+    // result = num1 % num2;
+    // console.log(result);
+
+    // let num1 = 10;
+    // let num2 = 20;
+    // let operator = '/';
+    // let result = 0;
+    // if(operator === '/');
+    // result = num1 / num2;
+    // console.log(result);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+     
+
+    
+        
+        
+
+    
+
+        
+
+    
+    
